c5f22dc5ad1d340aa96df7912620c4a7b269c7ff
shanepgit <shanemp@gmail.com>
2024-09-03 11:42:32 -0400

    testing

---
+++ db_scripts/db_import test.sql
@@ -0,0 +1,6 @@
+@import on;
+@import set ImportSource="/Users/shanep/Documents/git/openlibrary/data/processed/ol_dump_authors.csv"; 
+@import parse;
+@import target Table="authors";
+@import execute;
+@import off;
